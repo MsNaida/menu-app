8f573dbaffa8505ac6c0f5632fd569648aff8f98
Lisa Rodrigues <lisa.r.rodrigues@gmail.com>
2020-12-11 11:44:36 -0800

    removed studentId and teacherId from entity.Class

--- Application/src/Entity/Class.java
+++ Application/src/Entity/Class.java
@@ -3,14 +3,10 @@ package Entity;
 public class Class {
 
 	private int classId;
-	private int teacherId;
-	private int studentId;
 	private String className;
 	
 	public Class(int classId, int teacherId, int studentId, String className) {
 		this.setClassId(classId);
-		this.setTeacherId(teacherId);
-		this.setStudentId(studentId);
 		this.setClassName(className);
 	}
 	
@@ -20,23 +16,6 @@ public class Class {
 	public void setClassId(int classId) {
 		this.classId = classId;
 	}
-	
-	
-	public int getTeacherId() {
-		return teacherId;
-	}
-	public void setTeacherId(int teacherId) {
-		this.teacherId = teacherId;
-	}
-	
-	
-	public int getStudentId() {
-		return studentId;
-	}
-	public void setStudentId(int studentId) {
-		this.studentId = studentId;
-	}
-	
 	
 	public String getClassName() {
 		return className;
